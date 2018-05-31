b74f9f38f78cbb7629fd3cb3e81f0a505eeb0707
Jordan <jordantmarshall3@gmail.com>
2018-05-31 14:56:11 -0500

    redux setup

--- yak/src/index.js
+++ yak/src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom';
 import './global.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import reducer from './reducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Provider store={store}>
+            <MuiThemeProvider>
+                <App />
+            </MuiThemeProvider>
+        </Provider>
+    </BrowserRouter>,
+    document.getElementById('root'));
 registerServiceWorker();
