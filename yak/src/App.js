1b5168bbc792cb98ddfb7cdb7f51289fc4dae7d2
Jordan <jordantmarshall3@gmail.com>
2018-06-01 15:18:49 -0500

    sidebar nav and routing, removed unneccessary styles

--- yak/src/App.js
+++ yak/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './assets/logo/yak-logo-fullbody.svg';
 import './App.css';
 import Dash from './dashboard/Dash';
 
@@ -7,10 +6,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <Dash/>
       </div>
     );
---
+++ yak/src/dashboard/sidebar/Sidebar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import logo from '../../assets/logo/yak-logo-fullbody.svg';
+
+
+const Sidebar = () => (
+    <div className="sidebarComponent">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <h1 className="App-title">YAK Header</h1>
+
+        <ul style={{listStyleType: "none", padding: 0, color: "#fff"}}>
+            <Link to="/">
+                <li>
+                    Home
+                </li>
+            </Link>
+            <Link to="/export">
+                <li>
+                    Export
+                </li>
+            </Link>
+            <Link to="/settings">
+                <li>
+                    Settings
+                </li>
+            </Link>
+        </ul>
+    </div>
+);
+
+export default Sidebar;
