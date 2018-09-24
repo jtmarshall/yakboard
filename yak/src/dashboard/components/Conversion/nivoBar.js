9a93ed28d69f2c48ae66eec9904a25dead9160e4
Jordan <jordantmarshall3@gmail.com>
2018-09-24 11:23:23 -0500

    update line graph to update according to user selected date frames

--- yak/src/dashboard/components/Conversion/nivoBar.js
+++ yak/src/dashboard/components/Conversion/nivoBar.js
@@ -27,67 +27,39 @@ export default class NivoBar extends Component {
         this.state = {
             chartData: [
                 {
-                    "country": "AD",
-                    "hot dog": 33,
-                    "burger": 33,
-                    "sandwich": 7,
-                    "kebab": 129,
-                    "fries": 16,
-                    "donut": 18,
+                    "day": "9/1",
+                    "Call": 33,
+                    "5+ TOS": 33,
                 },
                 {
-                    "country": "AE",
-                    "hot dog": 15,
-                    "burger": 9,
-                    "sandwich": 71,
-                    "kebab": 93,
-                    "fries": 161,
-                    "donut": 146,
+                    "day": "9/2",
+                    "Call": 15,
+                    "5+ TOS": 9,
                 },
                 {
-                    "country": "AF",
-                    "hot dog": 33,
-                    "burger": 59,
-                    "sandwich": 1,
-                    "kebab": 155,
-                    "fries": 170,
-                    "donut": 34,
+                    "day": "9/3",
+                    "Call": 33,
+                    "5+ TOS": 59,
                 },
                 {
-                    "country": "AG",
-                    "hot dog": 84,
-                    "burger": 188,
-                    "sandwich": 140,
-                    "kebab": 199,
-                    "fries": 181,
-                    "donut": 191,
+                    "day": "9/4",
+                    "Call": 84,
+                    "5+ TOS": 188,
                 },
                 {
-                    "country": "AI",
-                    "hot dog": 149,
-                    "burger": 90,
-                    "sandwich": 64,
-                    "kebab": 160,
-                    "fries": 60,
-                    "donut": 21,
+                    "day": "9/5",
+                    "Call": 81,
+                    "5+ TOS": 149,
                 },
                 {
-                    "country": "AL",
-                    "hot dog": 178,
-                    "burger": 29,
-                    "sandwich": 21,
-                    "kebab": 139,
-                    "fries": 110,
-                    "donut": 164,
+                    "day": "9/6",
+                    "Call": 178,
+                    "5+ TOS": 29,
                 },
                 {
-                    "country": "AM",
-                    "hot dog": 53,
-                    "burger": 55,
-                    "sandwich": 67,
-                    "kebab": 74,
-                    "fries": 5,
-                    "donut": 102,
+                    "day": "9/7",
+                    "Call": 53,
+                    "5+ TOS": 55,
                 }
             ]
         }
@@ -108,14 +80,10 @@ export default class NivoBar extends Component {
                         <ResponsiveBar
                             data={nivoData}
                             keys={[
-                                "hot dog",
-                                "burger",
-                                "sandwich",
-                                "kebab",
-                                "fries",
-                                "donut"
+                                "Call",
+                                "5+ TOS"
                             ]}
-                            indexBy="country"
+                            indexBy="day"
                             margin={{
                                 "top": 50,
                                 "right": 130,
@@ -123,49 +91,15 @@ export default class NivoBar extends Component {
                                 "left": 60
                             }}
                             padding={0.3}
-                            colors="nivo"
+                            groupMode="grouped"
+                            colors={['#4EAF4A','#FF7F02', '#377EB8']}
                             colorBy="id"
-                            defs={[
-                                {
-                                    "id": "dots",
-                                    "type": "patternDots",
-                                    "background": "inherit",
-                                    "color": "#38bcb2",
-                                    "size": 4,
-                                    "padding": 1,
-                                    "stagger": true
-                                },
-                                {
-                                    "id": "lines",
-                                    "type": "patternLines",
-                                    "background": "inherit",
-                                    "color": "#eed312",
-                                    "rotation": -45,
-                                    "lineWidth": 6,
-                                    "spacing": 10
-                                }
-                            ]}
-                            fill={[
-                                {
-                                    "match": {
-                                        "id": "fries"
-                                    },
-                                    "id": "dots"
-                                },
-                                {
-                                    "match": {
-                                        "id": "sandwich"
-                                    },
-                                    "id": "lines"
-                                }
-                            ]}
                             borderColor="inherit:darker(1.6)"
                             axisBottom={{
                                 "orient": "bottom",
                                 "tickSize": 5,
                                 "tickPadding": 5,
                                 "tickRotation": 0,
-                                "legend": "country",
                                 "legendPosition": "center",
                                 "legendOffset": 36
                             }}
